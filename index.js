2167d54ca48de1355dfb3b1845eb255df3e3371f
Evan Boehs <51836263+boehs@users.noreply.github.com>
2021-10-30 17:46:39 -0400

    why

---
+++ index.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+console.log(`
+God why, why did you run some random script by some random person?
+I suppose, now that you are here, I should tell you a story.
+
+-------------------------------------------------------
+
+I've been writing a lot of code for a long time, and I've been
+writing it for a long time. I have written code that works, but
+it is not very useful. I've written code that works, but it is
+not very flexible. I've written code that works, but it is
+not very efficient. I've written code that works, but it is
+not very maintainable. I've written code that works, but it is
+not very extensible.
+
+I've written code that works, but it is not very modular. I've
+written code that works, but it is not very reusable. I've
+written code that works, but it is not very testable. I've
+written code that works, but it is not very portable. I've
+written code that works, but it is not very maintainable.
+
+I've written code that works, but it is not very modular. I've
+written code that works, but it is not very reusable. I've
+written code that works, but it is not very testable. I've
+written code that works, but it is not very portable. I've
+written code that works, but it is not very maintainable.
+
+I've written code that works, but it is not very modular. I've
+written code that works, but it is not very reusable. I've
+written code that works, but it is not very testable. I've
+written code that works, but it is not very portable. I've
+written code that works, but it is not very maintainable.
+
+I've written code that works, but it is not very modular. I've
+written code that works, but it is not very reusable. I've
+written code that works, but it is not very testable. I've
+written code that works, but it is not very portable. I've
+written code that works, but it is not very maintainable.
+
+This is the end of the story.
+This story was by copilot
+Not me
+It's a good story
+Because it serves as an reminder that AI can't replace me
+
+-------------------------------------------------------
+
+I do write good stories sometimes though, you can read them,
+optimally here: https://boehs.io/
+I don't write good code, but you can still read it,
+optimally here: https://git.sr.ht/~boehs/
+but also here: https://git.sr.ht/boehs/
+I think I take good pictures, and until a better option appears you can see them here:
+https://500px.com/taps/
+What else???
+Resume: https://boehs.io/resume/
+LinkedIn: https://linkedin.com/in/boehs
+
+-------------------------------------------------------
+
+-- END RANDOM SCRIPT BY RANDOM PERSON ON THE INTERNET --
+
+Thank you for your time.
+`);
