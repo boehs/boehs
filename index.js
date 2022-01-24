346b6da24e5f3bffd681c33f46cebfb8e8e0d9aa
Evan Boehs <evan@boehs.org>
2022-01-24 18:35:52 -0500

    Why are people downloading this

--- index.js
+++ index.js
@@ -1,61 +1,21 @@
 #!/usr/bin/env node
 
 console.log(`
-God why, why did you run some random script by some random person?
-I suppose, now that you are here, I should tell you a story.
+People's willingness to execute unknown files just because someone told them to is baffling
+to me, however this package continues to rack up a download a day and I thought it was fitting
+to actually put something insightful here.
+
+I'm Evan, I have been coding for some time now. I care greatly about the code I write and maintain,
+and if it helps you then you have made my day.
 
 -------------------------------------------------------
 
-I've been writing a lot of code for a long time, and I've been
-writing it for a long time. I have written code that works, but
-it is not very useful. I've written code that works, but it is
-not very flexible. I've written code that works, but it is
-not very efficient. I've written code that works, but it is
-not very maintainable. I've written code that works, but it is
-not very extensible.
-
-I've written code that works, but it is not very modular. I've
-written code that works, but it is not very reusable. I've
-written code that works, but it is not very testable. I've
-written code that works, but it is not very portable. I've
-written code that works, but it is not very maintainable.
-
-I've written code that works, but it is not very modular. I've
-written code that works, but it is not very reusable. I've
-written code that works, but it is not very testable. I've
-written code that works, but it is not very portable. I've
-written code that works, but it is not very maintainable.
-
-I've written code that works, but it is not very modular. I've
-written code that works, but it is not very reusable. I've
-written code that works, but it is not very testable. I've
-written code that works, but it is not very portable. I've
-written code that works, but it is not very maintainable.
-
-I've written code that works, but it is not very modular. I've
-written code that works, but it is not very reusable. I've
-written code that works, but it is not very testable. I've
-written code that works, but it is not very portable. I've
-written code that works, but it is not very maintainable.
-
-This is the end of the story.
-This story was by copilot
-Not me
-It's a good story
-Because it serves as an reminder that AI can't replace me
-
--------------------------------------------------------
-
-I do write good stories sometimes though, you can read them,
-optimally here: https://boehs.io/
-I don't write good code, but you can still read it,
-optimally here: https://git.sr.ht/~boehs/
-but also here: https://git.sr.ht/boehs/
+read my code optimally here: https://git.sr.ht/~boehs/
+but also here: https://github.com/boehs/
 I think I take good pictures, and until a better option appears you can see them here:
 https://500px.com/taps/
 What else???
-Resume: https://boehs.io/resume/
-LinkedIn: https://linkedin.com/in/boehs
+Website: https://boehs.org
 
 -------------------------------------------------------
 
